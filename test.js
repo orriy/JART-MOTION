00639b2d009750555d5e7bfef1c4e926c9dd9ee6
orriy <jeonghuigim63@gmail.com>
2022-12-01 12:14:35 +0900

    project03

---
+++ test.js
@@ -0,0 +1,16 @@
+var swiper1 = new Swiper(".slide1", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+  navigation: {
+    nextEl: ".next1",
+    prevEl: ".prev1",
+  },
+});
